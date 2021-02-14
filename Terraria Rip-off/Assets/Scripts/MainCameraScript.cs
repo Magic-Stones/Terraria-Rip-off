3d08172c83d1c0e83a26762f03dd9b6def627d58
Magic-Stones <davesolayao123@gmail.com>
2021-02-14 19:39:11 +0800

    Sprites added/remade!

---
+++ Terraria Rip-off/Assets/Scripts/MainCameraScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour
+{
+    public Transform cameraPosition;
+    public Camera cameraSize;
+    public Vector3 cameraOffset;
+
+    public float increaseCameraSize = 3.0f, 
+                 cameraSizeSpeed = 1.0f;
+
+    private bool enemiesDetected; 
+    private float defaultCameraSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultCameraSize = cameraSize.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraPosition.position = transform.position + cameraOffset;
+
+        if (enemiesDetected)
+        {
+            CameraSizeIncrease(true);   
+        }
+        else
+        {
+            CameraSizeIncrease(false);
+        }
+
+    }
+
+    public void CameraSizeIncrease(bool increaseSize)
+    {
+        if (increaseSize)
+        {
+            cameraSize.orthographicSize += cameraSizeSpeed * Time.deltaTime;
+
+            if (cameraSize.orthographicSize >= increaseCameraSize)
+            {
+                cameraSize.orthographicSize = increaseCameraSize;
+            }
+        }
+        else
+        {
+            cameraSize.orthographicSize -= cameraSizeSpeed * Time.deltaTime;
+
+            if (cameraSize.orthographicSize <= defaultCameraSize)
+            {
+                cameraSize.orthographicSize = defaultCameraSize;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("EnemyTag"))
+        {
+            enemiesDetected = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("EnemyTag"))
+        {
+            enemiesDetected = false;
+        }
+    }
+}
---
+++ Terraria Rip-off/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    private Vector2 movement;
+
+    public float moveSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePosition();
+    }
+
+    private void ProcessInputs()
+    {
+        /* 
+            "GetAxis" returns a value between 0 and 1.
+            "GetAxisRaw" returns a value of either 0 or 1.
+        */
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(xMove, yMove).normalized; // "normalized" returns the magnitude of 1 to a diagonal movement.
+    }
+
+    private void MovePosition()
+    {
+        rigidBody2D.MovePosition(rigidBody2D.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //rigidBody.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+    }
+}
